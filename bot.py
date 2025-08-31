056bd9dc2d80e000638b24debd1cb9067c30104d
Sheikh Mahfuzul Islam Rafi <skmahfuzulislamrafi@gmail.com>
2025-08-31 11:29:59 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+#from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -117,5 +118,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
